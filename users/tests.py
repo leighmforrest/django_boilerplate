79ad1d2c4958d04ca31788abbc907fd195aa9914
Leigh Forrest <leighmforrest@gmail.com>
2019-08-05 06:08:32 -0400

    homepage tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='rod',
+            email='rod@example.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'rod')
+        self.assertEqual(user.email, 'rod@example.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='clark',
+            email='clark@dailyplanet.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'clark')
+        self.assertEqual(admin_user.email, 'clark@dailyplanet.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
